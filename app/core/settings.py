074d9845451c8a5c5c9e413e181d732098605c6e
xorwise <xorwise@gmail.com>
2024-06-14 15:35:51 +0300

    added poetry, fixed template repo to satisfy task

--- app/core/settings.py
+++ app/core/settings.py
@@ -5,10 +5,6 @@ load_dotenv()
 
 
 class Settings(BaseSettings):
-    ACCESS_TOKEN_SECRET: str
-    REFRESH_TOKEN_SECRET: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
-    REFRESH_TOKEN_EXPIRE_MINUTES: int
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
